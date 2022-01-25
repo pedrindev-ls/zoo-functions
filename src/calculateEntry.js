521f12cf6456b2aba9ed6bccb0ad0d2514f298c0
Pedrin <pedro515vieira@gmail.com>
2022-01-25 19:20:16 -0300

    requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,37 +1,15 @@
 const data = require('../data/zoo_data');
 
-// const teste = [
-//   { name: 'Pedro', age: 5 },
-//   { name: 'Lucas', age: 5 },
-//   { name: 'Julia', age: 5 },
-//   { name: 'Maria', age: 18 },
-//   { name: 'João', age: 18 },
-//   { name: 'Rodolfo', age: 50 },
-// ];
-
 const counter = {
   adult: 0,
   child: 0,
   senior: 0,
 };
 
-// const testReal = (entrants) => {
-//   if (entrants === undefined) {
-//     return undefined;
-//   }
-//   if (entrants.length === undefined) {
-//     return undefined;
-//   }
-// };
-
 function countEntrants(entrants) {
-  // testReal(entrants);
-  if (entrants === undefined) {
-    return undefined;
-  }
-  if (entrants.length === undefined) {
-    return undefined;
-  }
+  counter.adult = 0;
+  counter.child = 0;
+  counter.senior = 0;
   entrants.forEach((element) => {
     if (element.age < 18) {
       counter.child += 1;
@@ -44,20 +22,17 @@ function countEntrants(entrants) {
   return counter;
 }
 
-// console.log(countEntrants(teste));
-
 function calculateEntry(entrants) {
-  const family = countEntrants(entrants);
-  if (family === undefined) {
+  if (entrants === undefined) {
     return 0;
   }
+  if (Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  countEntrants(entrants);
   const { adult, child, senior } = counter;
-  // console.log(adult, child, senior);
   const passValue = (adult * 49.99) + (child * 20.99) + (senior * 24.99);
-  // console.log(passValue);
   return passValue;
 }
-
-// calculateEntry(teste);
 
 module.exports = { calculateEntry, countEntrants };
