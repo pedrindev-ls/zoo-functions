72313ba809221695d62a53457b14e12abb1e22af
Pedrin <pedro515vieira@gmail.com>
2022-01-25 22:11:25 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,35 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+// const expected = {
+//   lions: 4,
+//   tigers: 2,
+//   bears: 3,
+//   penguins: 4,
+//   otters: 4,
+//   frogs: 2,
+//   snakes: 2,
+//   elephants: 4,
+//   giraffes: 6,
+// };
 
+function countAnimals(animal) {
+  const animalsCount = {};
+  const { species } = data;
+  if (animal === undefined) {
+    species.forEach((element) => {
+      animalsCount[element.name] = element.residents.length;
+    });
+    return animalsCount;
+  }
+  const searchAnimal = species.find((specie) => specie.name === animal.specie);
+  let animalFound = searchAnimal.residents.length;
+  const animalFoundArray = searchAnimal.residents;
+  const chaves = Object.keys(animal);
+  if (chaves.includes('sex')) {
+    const searchSexs = animalFoundArray.filter((unity) => unity.sex === animal.sex);
+    animalFound = searchSexs.length;
+  }
+
+  return animalFound;
+}
 module.exports = countAnimals;
