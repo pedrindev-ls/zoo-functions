0bbdd695d1c67872dd6fd62fb159678b7ba623ec
Pedrin <pedro515vieira@gmail.com>
2022-01-26 19:25:36 -0300

    requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
+const { species, employees } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const worker = employees.find((element) => element.id === id);
+  const workerFound = worker;
+  const animal = workerFound.responsibleFor[0];
+  const findAnimal = species.find((element) => element.id === animal);
+  const animalFound = findAnimal.residents;
+  const ordenatedAnimals = animalFound.sort((a, b) => b.age - a.age);
+  const oldestAnimal = ordenatedAnimals[0];
+  const values = Object.values(oldestAnimal);
+  return values;
 }
 
 module.exports = getOldestFromFirstSpecies;
