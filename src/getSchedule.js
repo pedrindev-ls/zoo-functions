8334475fe23efc0814897a0ffd93ab3557d8a141
Pedrin <pedro515vieira@gmail.com>
2022-01-26 20:20:38 -0300

    iniciando requisito 8

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,5 +1,9 @@
 const data = require('../data/zoo_data');
 
+// const { species } = data;
+// const { hours } = data;
+// const { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday } = hours;
+
 function getSchedule(scheduleTarget) {
   // seu código aqui
 }
