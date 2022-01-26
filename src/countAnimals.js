4a351995ce6a1cc6f6a14f3410bed3d797c22a59
Pedrin <pedro515vieira@gmail.com>
2022-01-25 22:12:31 -0300

    requisito 5 correçoes

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,17 +1,5 @@
 const data = require('../data/zoo_data');
 
-// const expected = {
-//   lions: 4,
-//   tigers: 2,
-//   bears: 3,
-//   penguins: 4,
-//   otters: 4,
-//   frogs: 2,
-//   snakes: 2,
-//   elephants: 4,
-//   giraffes: 6,
-// };
-
 function countAnimals(animal) {
   const animalsCount = {};
   const { species } = data;
