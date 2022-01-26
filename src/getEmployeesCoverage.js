14b6bdbf1cd73ba5b3f26b698e80837978e09e0f
Pedrin <pedro515vieira@gmail.com>
2022-01-26 18:39:48 -0300

    requisito 4

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+// {
+//   "id": "c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1",
+//   "fullName": "Nigel Nelson",
+//   "species": [ "lions", "tigers" ],
+//   "locations": [ "NE", "NW" ],
+// },
+
+function getEmployeesCoverage(value) {
+  // guenta ai
 }
 
 module.exports = getEmployeesCoverage;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,32 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  const conferManager = employees.some((element) => element.managers.some((value) => value === id));
+  return conferManager;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const subWorkers = [];
+  employees.forEach((element) => {
+    const values = element.managers;
+    if (values.includes(managerId)) {
+      subWorkers.push(`${element.firstName} ${element.lastName}`);
+    }
+    // console.log(subWorkers);
+  });
+
+  try {
+    if (!isManager(managerId)) {
+      throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+    }
+  } catch (error) {
+    throw error.message;
+  }
+  return subWorkers;
 }
+
+// getRelatedEmployees('fdb2543b-5662-46a7-badc-93d960fdc0a8');
 
 module.exports = { isManager, getRelatedEmployees };
